ff18c4847528c996cd1e337c4c91b2d48df06928
cristinavoda <vodacristina7@gmail.com>
2024-06-18 10:33:08 +0200

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+  <div>
+<h1 class ="text-3xl text-green font bold
+underline">
+"My proyect!"
+  </h1>
+  </div>
+  
+</template>
+<script>
+  export default {
+    name:"App",
+    data(){
+      return{title :""}
+    }
+  }
+
+</script>
